3d1febd65bcb21266700186fe6946f2ebaab8eec
Enes Baytemir <enesbytmr@gmail.com>
2022-11-12 01:58:12 +0300

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/about' => 'home#about'
+  get '/testimonials' => 'home#testimonials'
+  get '/login' => 'home#login'
+  get '/contact' => 'home#contact'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
